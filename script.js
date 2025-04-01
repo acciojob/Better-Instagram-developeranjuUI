b3bc52a3cfc115036a6313bccb353cf6b41d4caa
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-04-01 15:09:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your code here
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll(".image");
+
+    images.forEach(img => {
+        img.setAttribute("draggable", true);
+
+        img.addEventListener("dragstart", (event) => {
+            event.dataTransfer.setData("id", event.target.id);
+        });
+
+        img.addEventListener("dragover", (event) => {
+            event.preventDefault();
+        });
+
+        img.addEventListener("drop", (event) => {
+            event.preventDefault();
+            let draggedElementId = event.dataTransfer.getData("id");
+            let draggedElement = document.getElementById(draggedElementId);
+
+            if (draggedElement && draggedElement !== event.target) {
+                
+                let tempBg = window.getComputedStyle(draggedElement).backgroundImage;
+                draggedElement.style.backgroundImage = window.getComputedStyle(event.target).backgroundImage;
+                event.target.style.backgroundImage = tempBg;
+            }
+        });
+    });
+});
